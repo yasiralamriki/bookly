2cecbd7d0b790d38f51f15f0bf68538501bb1e80
ياسر الأمريكي <70416702+yasiralamriki@users.noreply.github.com>
2025-07-27 14:59:22 -0400

    feat: add navigation menu and separator components; update App layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import './App.css'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+import { Separator } from "@/components/ui/separator"
 
 function App() {
 
   return (
     <>
-      <p>
-        Placeholder
-      </p>
+      <div id="navbar-container" className="flex justify-between items-center self-stretch px-16 py-4">
+        <div id="logo" className="flex items-center h-8 gap-2">
+          <img src="/src/assets/logo.svg" alt="Bookly Logo" className="h-8 w-8 inline-block" />
+          <span className="text-lg font-semibold">Bookly</span>
+        </div>
+        <div id="navbar" className="flex items-center gap-4">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/">Home</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/settings">Settings</NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <Separator orientation="vertical" className="!h-6" />
+        </div>
+      </div>
     </>
   )
 }
